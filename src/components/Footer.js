8821eaaf849b91dd3328e83bdca42a94d2831afe
Eric Huberman <ehuber0601@gmail.com>
2021-11-09 16:41:38 -0500

    finished styling my page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 function Footer(props) {
-    return <h1>Footer</h1>;
+    return <div className="Footer">
+        <p>Copyright Eric Huberman &copy; | <a href="https://www.linkedin.com/in/eric-huberman-929912184/">LinkedIn</a></p>
+    </div>;
   }
   
   export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
 function Home(props) {
-    return <h1>Home</h1>;
+    return <div className="content">
+        <h1>Welcome!</h1>
+        <p>Welcome to my portfolio page! Here, you get to see all the projects that I've worked on in this course.</p>
+        <div className="memes">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/MVV_HXtEbLo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <img src="https://qph.fs.quoracdn.net/main-qimg-8bc8149821db4e32687f790f85501bd5"/>
+        </div>
+    </div> 
+    
   }
   
   export default Home;
